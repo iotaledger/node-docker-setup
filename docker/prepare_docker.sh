0ec1dd356dd42bbc117128dc6fbbf4c3f21dbe68
muXxer <git@muxxer.de>
2022-09-21 16:38:50 +0200

    Initial commit

---
+++ docker/prepare_docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ ! -f .env ]; then
+  echo "No .env file found. Please see README.md for more details"
+fi
+
+if [[ "$OSTYPE" != "darwin"* && "$EUID" -ne 0 ]]; then
+  echo "Please run as root or with sudo"
+  exit
+fi
+
+# Prepare db directory
+mkdir -p data
+mkdir -p data/grafana
+mkdir -p data/prometheus
+mkdir -p data/dashboard
+mkdir -p data/wasp
+if [[ "$OSTYPE" != "darwin"* ]]; then
+  chown -R 65532:65532 data
+  chown 65532:65532 peering.json
+fi
