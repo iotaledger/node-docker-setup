48a5f68727d1f444224d6dad5a3cb2c7b6c4936c
S3bastian <6882236+S3bastian@users.noreply.github.com>
2024-03-06 14:04:28 +0100

    A query appears before the update

--- shimmer/update.sh
+++ shimmer/update.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker compose down && docker compose pull && docker compose up -d
+
+while true; do
+    read -p "Should the existing Hornet installation be updated (existing containers are recreated)? " yn
+    case $yn in
+        [Yy]* ) docker compose down && docker compose pull && docker compose up -d; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
